f9e04c8d72abcfd36216c8d1f8a640dd6da72419
habdullah <14bscshabdullah>
2020-08-12 20:02:18 +0200

    Pulling changes and adding new SD test

--- cmd/selectivedeployment/main.go
+++ cmd/selectivedeployment/main.go
@@ -24,6 +24,16 @@ import (
 func main() {
 	// Set kubeconfig to be used to create clientsets
 	bootstrap.SetKubeConfig()
+	clientset, err := bootstrap.CreateClientSet()
+	if err != nil {
+		log.Println(err.Error())
+		panic(err.Error())
+	}
+	edgenetClientset, err := bootstrap.CreateEdgeNetClientSet()
+	if err != nil {
+		log.Println(err.Error())
+		panic(err.Error())
+	}
 	// Start the controller to provide the functionalities of selectivedeployment resource
-	selectivedeployment.Start()
+	selectivedeployment.Start(clientset, edgenetClientset)
 }
--- pkg/controller/v1alpha/selectivedeployment/selectivedeployment_test.go
+++ pkg/controller/v1alpha/selectivedeployment/selectivedeployment_test.go
@@ -3,7 +3,6 @@ package selectivedeployment
 import (
 	apps_v1alpha "edgenet/pkg/apis/apps/v1alpha"
 	"edgenet/pkg/client/clientset/versioned"
-	"fmt"
 	"io/ioutil"
 	"os"
 	"testing"
@@ -28,10 +27,9 @@ var errorDict = map[string]string{
 }
 
 type SDTestGroup struct {
-	authorityObj  apps_v1alpha.Authority
 	client        kubernetes.Interface
-	nodeObj       corev1.Node
-	userObj       apps_v1alpha.User
+	nodeFRObj     corev1.Node
+	nodeUSObj     corev1.Node
 	sdObj         apps_v1alpha.SelectiveDeployment
 	edgenetclient versioned.Interface
 	handler       SDHandler
@@ -51,74 +49,79 @@ func (g *SDTestGroup) Init() {
 			APIVersion: "apps.edgenet.io/v1alpha",
 		},
 		ObjectMeta: metav1.ObjectMeta{
-			ClusterName: "Edgenet",
-			Name:        "edgenetSD",
-			Namespace:   "authority-edgenet",
+			Name: "country",
+			//Namespace: "authority-edgenet",
 		},
 		Spec: apps_v1alpha.SelectiveDeploymentSpec{
 			Controllers: apps_v1alpha.Controllers{
-				Deployment: []v1.Deployment{v1.Deployment{
-					TypeMeta: metav1.TypeMeta{
-						Kind:       "Deployment",
-						APIVersion: "apps/v1",
-					},
-					ObjectMeta: metav1.ObjectMeta{
-						Namespace: "authority-edgenet",
-						Name:      "deploymentTest",
-						UID:       "UIDTest",
-					},
-					Status: v1.DeploymentStatus{
-						Conditions: []v1.DeploymentCondition{
-							v1.DeploymentCondition{
-								Type:   success,
-								Status: "true",
+				Deployment: []v1.Deployment{
+					v1.Deployment{
+						TypeMeta: metav1.TypeMeta{
+							Kind:       "Deployment",
+							APIVersion: "apps/v1",
+						},
+						ObjectMeta: metav1.ObjectMeta{
+							Name: "deployment1",
+							//Namespace: "authority-edgenet",
+							Labels: map[string]string{
+								"app": "nginx",
+							},
+						},
+						Spec: v1.DeploymentSpec{
+							Selector: &metav1.LabelSelector{
+								MatchLabels: map[string]string{
+									"app": "nginx",
+								},
+							},
+							Template: corev1.PodTemplateSpec{
+								ObjectMeta: metav1.ObjectMeta{
+									Labels: map[string]string{
+										"app": "nginx",
+									},
+								},
+								Spec: corev1.PodSpec{
+									Containers: []corev1.Container{
+										corev1.Container{
+											Name:  "nginx",
+											Image: "nginx:1.7.9",
+											Ports: []corev1.ContainerPort{
+												corev1.ContainerPort{
+													ContainerPort: 80,
+												},
+											},
+										},
+									},
+								},
 							},
 						},
 					},
-					Spec: v1.DeploymentSpec{
-						// Selector: &metav1.LabelSelector{
-						// 	MatchLabels: map[string]string{"testLabel": "testLabel"},
-						// },
-					},
 				},
-				},
-				// DaemonSet: v1.DaemonSet{
-				// TypeMeta: metav1.TypeMeta{
-				// 	Kind:       "DeamonSet",
-				// 	APIVersion: "apps/v1",
-				// },
-				// ObjectMeta: metav1.ObjectMeta{
-				// 	Namespace: "authority-edgenet",
-				// 	Name:      "TestDeamonSet",
-				// },
-				// Spec: v1.DaemonSetSpec{
-				// },
-				// Status: v1.DaemonSetStatus{
-				// },
-				// },
 			},
-
 			Selector: []apps_v1alpha.Selector{
-				apps_v1alpha.Selector{
-					Name:     "city",
-					Value:    []string{"Islamabad"},
+				{
+					Value:    []string{"FR"},
 					Operator: "In",
+					Quantity: 1,
+					Name:     "country",
 				},
 			},
 		},
 	}
-	nodeObj := corev1.Node{
+	nodeFRObj := corev1.Node{
 		TypeMeta: metav1.TypeMeta{
 			Kind:       "Node",
 			APIVersion: "apps.edgenet.io/v1alpha",
 		},
 		ObjectMeta: metav1.ObjectMeta{
-			Name: "edgenetNode",
-			// Namespace: "authority-edgnet",
-			Labels: map[string]string{"kubernetes.io/hostname": "nodeHostName", "edge-net.io/city": "Islamabad", "edge-net.io/lon": "11.11", "edge-net.io/lat": "22.22"},
-		},
-		Spec: corev1.NodeSpec{
-			Unschedulable: false,
+			Name: "edgenet.planet-lab.eu",
+			//Namespace: "authority-edgenet",
+			Labels: map[string]string{
+				"edge-net.io/hostname":    "edgenet.planet-lab.eu",
+				"edge-net.io/city":        "Paris",
+				"edge-net.io/country-iso": "FR",
+				"edge-net.io/state-iso":   "FR",
+				"edge-net.io/continent":   "Europe",
+			},
 		},
 		Status: corev1.NodeStatus{
 			Conditions: []corev1.NodeCondition{
@@ -129,68 +132,36 @@ func (g *SDTestGroup) Init() {
 			},
 		},
 	}
-	authorityObj := apps_v1alpha.Authority{
+	nodeUSObj := corev1.Node{
 		TypeMeta: metav1.TypeMeta{
-			Kind:       "Authority",
+			Kind:       "Node",
 			APIVersion: "apps.edgenet.io/v1alpha",
 		},
 		ObjectMeta: metav1.ObjectMeta{
-			Name: "edgenet",
+			Name: "maxgigapop-1.edge-net.io",
+			//Namespace: "authority-edgenet",
+			Labels: map[string]string{
+				"edge-net.io/hostname":    "maxgigapop-1.edge-net.io",
+				"edge-net.io/city":        "College Park",
+				"edge-net.io/country-iso": "US",
+				"edge-net.io/state-iso":   "MD",
+				"edge-net.io/continent":   "North America",
+			},
 		},
-		Spec: apps_v1alpha.AuthoritySpec{
-			FullName:  "EdgeNet",
-			ShortName: "EdgeNet",
-			URL:       "https://www.edge-net.org",
-			Address: apps_v1alpha.Address{
-				City:    "Paris - NY - CA",
-				Country: "France - US",
-				Street:  "4 place Jussieu, boite 169",
-				ZIP:     "75005",
+		Status: corev1.NodeStatus{
+			Conditions: []corev1.NodeCondition{
+				corev1.NodeCondition{
+					Type:   "Ready",
+					Status: "True",
+				},
 			},
-			Contact: apps_v1alpha.Contact{
-				Email:     "unittest@edge-net.org",
-				FirstName: "unit",
-				LastName:  "testing",
-				Phone:     "+33NUMBER",
-				Username:  "unittesting",
-			},
-			Enabled: false,
 		},
 	}
-	userObj := apps_v1alpha.User{
-		TypeMeta: metav1.TypeMeta{
-			Kind:       "User",
-			APIVersion: "apps.edgenet.io/v1alpha",
-		},
-		ObjectMeta: metav1.ObjectMeta{
-			Name: "unittesting",
-		},
-		Spec: apps_v1alpha.UserSpec{
-			FirstName: "EdgeNet",
-			LastName:  "EdgeNet",
-			Email:     "unittest@edge-net.org",
-			Active:    true,
-		},
-		Status: apps_v1alpha.UserStatus{
-			Type:  "Admin",
-			State: success,
-		},
-	}
-	g.nodeObj = nodeObj
-	g.authorityObj = authorityObj
-	g.userObj = userObj
+	g.nodeFRObj = nodeFRObj
+	g.nodeUSObj = nodeUSObj
 	g.sdObj = sdObj
 	g.client = testclient.NewSimpleClientset()
 	g.edgenetclient = edgenettestclient.NewSimpleClientset()
-	// Create Authority
-	g.edgenetclient.AppsV1alpha().Authorities().Create(g.authorityObj.DeepCopy())
-	g.authorityObj.Status.State = success
-	g.authorityObj.Spec.Enabled = true
-	// Update Authority status
-	g.edgenetclient.AppsV1alpha().Authorities().UpdateStatus(g.authorityObj.DeepCopy())
-	authorityChildNamespace := &corev1.Namespace{ObjectMeta: metav1.ObjectMeta{Name: fmt.Sprintf("authority-%s", g.authorityObj.GetName())}}
-	// Create Authority child namepace
-	g.client.CoreV1().Namespaces().Create(authorityChildNamespace)
 }
 
 // TestHandlerInit for handler initialization
@@ -212,8 +183,18 @@ func TestObjectCreated(t *testing.T) {
 	g := SDTestGroup{}
 	g.Init()
 	g.handler.Init(g.client, g.edgenetclient)
-	g.client.CoreV1().Nodes().Create(g.nodeObj.DeepCopy())
-	t.Run("creation of SD", func(t *testing.T) {
+	// Creating two nodes
+	g.client.CoreV1().Nodes().Create(g.nodeFRObj.DeepCopy())
+	g.client.CoreV1().Nodes().Create(g.nodeUSObj.DeepCopy())
+
+	t.Run("creation of SD, Deployment as controller", func(t *testing.T) {
+		// Invoking the Create function of SD
+		g.edgenetclient.AppsV1alpha().SelectiveDeployments("").Create(g.sdObj.DeepCopy())
 		g.handler.ObjectCreated(g.sdObj.DeepCopy())
+		// Get the selectiveDeployment
+		sd, _ := g.edgenetclient.AppsV1alpha().SelectiveDeployments("").Get(g.sdObj.GetName(), metav1.GetOptions{})
+		if sd.Status.State != success {
+			t.Errorf("Creating selective deployment failed")
+		}
 	})
 }
